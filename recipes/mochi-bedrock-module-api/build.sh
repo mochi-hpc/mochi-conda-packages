8a729fe76fb6ca88ffb6708907d5cda22124fc7a
Matthieu Dorier <mdorier@anl.gov>
2026-01-30 14:03:42 +0000

    added recipe for mochi-bedrock-module-api

---
+++ recipes/mochi-bedrock-module-api/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+# Set pkg-config path for host dependencies
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+mkdir build && cd build
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_TESTS=OFF \
+    -DENABLE_EXAMPLES=OFF \
+    ..
+
+make -j${CPU_COUNT}
+make install
