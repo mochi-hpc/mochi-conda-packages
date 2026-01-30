cebbeecdf80fceec75e0dc5695f381d30eec8837
Matthieu Dorier <mdorier@anl.gov>
2026-01-30 17:15:53 +0000

    added mochi-bedrock recipe

---
+++ recipes/mochi-bedrock/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# Set pkg-config path for host dependencies
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# Convert true/false to ON/OFF for CMake
+if [ "$flock" = "true" ]; then FLOCK=ON; else FLOCK=OFF; fi
+if [ "$use_mpi" = "true" ]; then MPI=ON; else MPI=OFF; fi
+
+mkdir build && cd build
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_FLOCK=${FLOCK} \
+    -DENABLE_MPI=${MPI} \
+    -DENABLE_PYTHON=ON \
+    -DENABLE_SPACE=OFF \
+    -DENABLE_TESTS=OFF \
+    -DENABLE_EXAMPLES=OFF \
+    ..
+
+make -j${CPU_COUNT}
+make install
