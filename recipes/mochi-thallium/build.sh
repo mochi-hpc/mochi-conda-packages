a3b5a466448f39e00b87804c4adde00c2bf78ee6
Matthieu Dorier <mdorier@anl.gov>
2026-01-30 12:49:28 +0000

    added mochi-thallium recipe

---
+++ recipes/mochi-thallium/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# Set pkg-config path for host dependencies
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+mkdir build && cd build
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DENABLE_TESTS=OFF \
+    -DENABLE_EXAMPLES=OFF \
+    ..
+
+make -j${CPU_COUNT}
+make install
