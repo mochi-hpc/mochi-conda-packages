9f9965c2641342c7c989bc3d735cd6357791d03d
Matthieu Dorier <mdorier@anl.gov>
2026-02-04 09:50:48 +0000

    fixed pmdk build on linux-64

--- recipes/pmdk/build.sh
+++ recipes/pmdk/build.sh
@@ -5,17 +5,25 @@ set -ex
 export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
+# Detect architecture (pmdk Makefile doesn't always detect x86_64 correctly)
+ARCH_FLAG=""
+if [ "$(uname -m)" = "x86_64" ]; then
+    ARCH_FLAG="ARCH=x86_64"
+fi
+
 # Build PMDK (without docs and without ndctl/daxctl)
 make -j${CPU_COUNT} \
     prefix=$PREFIX \
     NDCTL_ENABLE=n \
     BUILD_EXAMPLES=n \
     BUILD_BENCHMARKS=n \
-    DOC=n
+    DOC=n \
+    $ARCH_FLAG
 
 make install \
     prefix=$PREFIX \
     NDCTL_ENABLE=n \
     BUILD_EXAMPLES=n \
     BUILD_BENCHMARKS=n \
-    DOC=n
+    DOC=n \
+    $ARCH_FLAG
